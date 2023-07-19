e07386a9236ffd0af167021ec75bdc4972ec8fc7
IVB77 <ivshinvb@mail.ru>
2023-07-19 23:41:09 +0500

    Change for Lesson Animation 1 Exercise 1

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,8 +1,8 @@
 package ru.netology.statsview
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data= listOf(
+        val view = findViewById<StatsView>(R.id.statsView)
+        view.data = listOf(
             500F,
             500F,
             500F,
             500F,
         )
         findViewById<StatsView>(R.id.statsView).pctTotal = 50F //Сколько процентов занимают данные
+
     }
 }
--- app/src/main/java/ru/netology/statsview/ui/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -1,5 +1,6 @@
 package ru.netology.statsview.ui
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -7,10 +8,10 @@ import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.content.withStyledAttributes
 import ru.netology.statsview.R
 import ru.netology.statsview.utils.AndroidUtils
-
 import kotlin.math.min
 import kotlin.random.Random
 
@@ -42,17 +43,19 @@ class StatsView @JvmOverloads constructor(
                 getColor(R.styleable.StatsView_color3, generateRandomColor()),
                 getColor(R.styleable.StatsView_color4, generateRandomColor()),
             )
-            colorEmpty=getColor(R.styleable.StatsView_colorEmpty, 0xFFFFFFFF.toInt())
+            colorEmpty = getColor(R.styleable.StatsView_colorEmpty, 0xFFFFFFFF.toInt())
         }
     }
 
+    private var progress = 0F
+    private var startAngle = -90F
+    private var valueAnimator: ValueAnimator? = null
     var data: List<Float> = emptyList()
         set(value) {
             field = value
-            invalidate()
+            update()
         }
     var pctTotal = 100F
-
 
 
     private var radius = 0F
@@ -89,44 +92,77 @@ class StatsView @JvmOverloads constructor(
         if (data.isEmpty()) {
             return
         }
-        val dataOfShare : List<Float> = changeToShare(data,pctTotal)
-        var startAngle = -90F
-        dataOfShare.forEachIndexed() { index, datum ->
+        val dataOfShare: List<Float> = changeToShare(data, pctTotal)
+
+        dataOfShare.forEachIndexed { index, datum ->
             val angle = datum * 360F
-            if(dataOfShare.lastIndex==index && pctTotal<100){
+            if (dataOfShare.lastIndex == index && pctTotal < 100) {
                 paint.color = colorEmpty
-            }
-            else {
+            } else {
                 paint.color = colors.getOrElse(index) { generateRandomColor() }
             }
-            canvas.drawArc(oval, startAngle, angle, false, paint)
+            canvas.drawArc(oval, startAngle * progress, angle * progress, false, paint)
             startAngle += angle
         }
 
-        paint.color = colors.first()
-        canvas.drawPoint(center.x,center.y-radius,paint)
+        paint.color = colors.first() * progress.toInt()
+        canvas.drawPoint(center.x, center.y - radius, paint)
         canvas.drawText(
-            "%.2f%%".format(pctTotal),
+            "%.2f%%".format(pctTotal * progress),
             center.x,
             center.y + textPaint.textSize / 4,
             textPaint
         )
 
     }
+
+    private fun update() {
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 3000
+            interpolator = LinearInterpolator()
+
+        }.also {
+            it.start()
+        }
+
+        valueAnimator = ValueAnimator.ofFloat(-90F, 270F).apply {
+            addUpdateListener { anim ->
+                startAngle = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 3000
+            interpolator = LinearInterpolator()
+
+        }.also {
+            it.start()
+        }
+
+
+    }
 }
 
 fun changeToShare(data: List<Float>, pctTotal: Float): List<Float> {
-    if (pctTotal>=100F) {
+    return if (pctTotal >= 100F) {
         var dataOfShare = emptyList<Float>()
         data.forEach { dataOfShare = dataOfShare + (it / data.sum()) }
-        return dataOfShare
-    }
-    else{
+        dataOfShare
+    } else {
         var dataOfShare = emptyList<Float>()
-        val sum = data.sum()/pctTotal*100
+        val sum = data.sum() / pctTotal * 100
         data.forEach { dataOfShare = dataOfShare + (it / sum) }
-        dataOfShare = dataOfShare + (100-pctTotal)/100
-        return dataOfShare
+        dataOfShare = dataOfShare + (100 - pctTotal) / 100
+        dataOfShare
     }
 }
+
 fun generateRandomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+
